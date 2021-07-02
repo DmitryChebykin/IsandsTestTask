39f6cee18161dbd51bf93807047d94d1887b54d9
DmitryChebykin <78965652+DmitryChebykin@users.noreply.github.com>
2021-07-02 12:59:56 +0700

    init commit to develop add generated by IDE pom.xml, test and app classes, application.properties

---
+++ src/main/java/com/example/isandstesttask/IsandsTestTaskApplication.java
@@ -0,0 +1,12 @@
+package com.example.isandstesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsandsTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsandsTestTaskApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/isandstesttask/IsandsTestTaskApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.isandstesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsandsTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
