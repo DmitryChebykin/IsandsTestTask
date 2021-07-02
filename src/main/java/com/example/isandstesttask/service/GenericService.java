a2f1be9d833311e04ab8b1e3cf04fc725bb7695a
DmitryChebykin <78965652+DmitryChebykin@users.noreply.github.com>
2021-07-03 02:48:22 +0700

    add base service

---
+++ src/main/java/com/example/isandstesttask/service/GenericService.java
@@ -0,0 +1,30 @@
+package com.example.isandstesttask.service;
+
+import com.example.isandstesttask.entity.BaseProduct;
+import com.example.isandstesttask.repository.GenericProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class GenericService {
+    private GenericProductRepository<BaseProduct> genericProductRepository;
+
+    @Autowired
+    public GenericService(GenericProductRepository genericProductRepository) {
+        this.genericProductRepository = genericProductRepository;
+    }
+
+    public GenericService() {
+
+    }
+
+    public List<BaseProduct> getProductByModelName(String modelName)  {
+        return genericProductRepository.findAllByModelNameAllIgnoreCase(modelName).orElseThrow(() -> new ProductNotFoundException("Can't find products with model name: " + modelName));
+    }
+
+    public List<BaseProduct> getProductByProductType(String productType) {
+        return genericProductRepository.findAllByProductType(productType)
+                .orElseThrow(() -> new ProductNotFoundException("Can't find products with product type: " + productType));
+    }
+}
