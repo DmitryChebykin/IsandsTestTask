35fcfc8db5163b1b5f002d80883968fef9896396
DmitryChebykin <78965652+DmitryChebykin@users.noreply.github.com>
2021-07-09 19:26:39 +0700

    fix controller path

--- src/main/java/com/example/isandstesttask/controller/PcBoxController.java
+++ src/main/java/com/example/isandstesttask/controller/PcBoxController.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/product/PcBox")
+@RequestMapping("/product/pcbox")
 public class PcBoxController {
     private final com.example.isandstesttask.service.PcBoxService PcBoxService;
     private final BrandService brandService;
